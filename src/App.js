3cff5e045870758efda4ac3a366c30ebd1b5b1ee
Mariam Nersisyan <mariamnersisyan97@gmail.com>
2022-08-24 18:04:20 -0400

    render login

--- src/App.js
+++ src/App.js
@@ -1,20 +1,63 @@
+import { IconButton } from "@material-ui/core";
 import * as React from "react";
 import { Routes, Route, Link } from "react-router-dom";
 import "./App.css";
 import Home from "./Home";
+import { Login } from "./Login";
 import Navbar from "./Navbar";
 import SongLibrary from "./SongLibrary";
 import Submit from "./Submit";
-
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import { useState } from "react";
 function App() {
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+    },
+  }));
+  const classes = useStyles();
+
+  const [loginRender, setLoginRender] = useState([]);
+  const loginOnClick = () => {
+    setLoginRender((login) => [...login, <Login />]);
+    console.log("login");
+  };
+
   return (
     <div className="App">
-      <h1>Jukebox</h1>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          ></IconButton>
+          <Typography variant="h6" className={classes.title}>
+            Jukebox
+          </Typography>
+          <Button onClick={loginOnClick} color="inherit">
+            Login
+          </Button>
+        </Toolbar>
+      </AppBar>
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="navbar" element={<Navbar />} />
         <Route path="library" element={<SongLibrary />} />
         <Route path="submit" element={<Submit />} />
+        <Route path="login" element={<Login />} />
       </Routes>
     </div>
   );
--- src/Home.js
+++ src/Home.js
@@ -1,30 +1,9 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import { TextField } from "@material-ui/core";
-import Button from "@material-ui/core/Button";
 
 const Home = () => {
-  const useStyles = makeStyles((theme) => ({
-    root: {
-      "& > *": {
-        margin: theme.spacing(1),
-        width: "25ch",
-      },
-    },
-  }));
-  const classes = useStyles();
   return (
     <div>
       <h1> Home</h1>
-      <h3>Login</h3>
-      <form className={classes.root} noValidate autoComplete="off">
-        <TextField id="outlined-basic" label="First Name" variant="outlined" />
-        <TextField id="outlined-basic" label="Last Name" variant="outlined" />
-        <TextField id="outlined-basic" label="Email" variant="outlined" />
-      </form>
-      <Button variant="outlined" color="secondary">
-        Login
-      </Button>
     </div>
   );
 };
